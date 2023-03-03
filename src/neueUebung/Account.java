2437deb0aefa6a029c83adfd147999aff6c4b4c8
eggerteresa <teresa.egger@gmx.net>
2023-03-03 12:13:10 +0100

    for Loop

--- src/neueUebung/Account.java
+++ src/neueUebung/Account.java
@@ -5,21 +5,27 @@ public class Account {
     public static void main(String[] args) {
         int years = 10;
         double investment = 2000;
-        double interestRate = 102.5;
+        double interestRate = 102.5; // oder 0.0025 ( accbalance + accBalance *interestrate. oder 1.025 (dann malrechnen)
         accountBalance(years, investment, interestRate);
 
 
     }
 
-    public static void accountBalance(int years, double investment, double interestRate) {
-        System.out.println(investment);
-        for (int i = 0; i <= years; i = i + 1) {
+    public static double accountBalance(int years, double investment, double interestRate) {
+       // System.out.println(investment);
 
-        investment = investment / 100 * interestRate;
-            System.out.println(investment);
+
+
+        double accountBal = investment;
+
+        for (int i = 1; i <= years; i = i + 1) { //was soll der for Loop machen? für jedes Jahr unsere AccountBalance berechnen
+
+        accountBal = accountBal / 100 * interestRate;
+            System.out.println( accountBal +"Euros in year " + i);
 
 
         }
+        return accountBal;
 
 
 
